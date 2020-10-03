7eba7d019d84f4068150c1807a32d19a0d7aaf52
risat01 <65094494+risat01@users.noreply.github.com>
2020-10-03 19:33:36 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,188 @@
+(function ($) {
+	"use strict";
+
+	/* ..............................................
+	Loader 
+	................................................. */
+
+	$(window).on('load', function () {
+		$('.preloader').fadeOut();
+		$('#preloader').delay(550).fadeOut('slow');
+		$('body').delay(450).css({ 'overflow': 'visible' });
+	});
+
+	/* ..............................................
+	Navbar Bar
+	................................................. */
+
+	$('.navbar-nav .nav-link').on('click', function () {
+		var toggle = $('.navbar-toggler').is(':visible');
+		if (toggle) {
+			$('.navbar-collapse').collapse('hide');
+		}
+	});
+
+	/* ..............................................
+	Fixed Menu
+	................................................. */
+
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
+
+	/* ..............................................
+	Properties Filter
+	................................................. */
+	var Container = $('.container');
+	Container.imagesLoaded(function () {
+		var portfolio = $('.properties-menu');
+		portfolio.on('click', 'button', function () {
+			$(this).addClass('active').siblings().removeClass('active');
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: filterValue
+			});
+		});
+		var $grid = $('.properties-list').isotope({
+			itemSelector: '.properties-grid'
+		});
+
+	});
+
+	/* ..............................................
+	Gallery
+	................................................. */
+
+	$(document).ready(function () {
+		$('.popup-gallery').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function (item) {
+					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+				}
+			}
+		});
+	});
+
+	/* ..............................................
+	Scroll To Top
+	................................................. */
+
+	$(document).ready(function () {
+
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#scroll-to-top').fadeIn();
+			} else {
+				$('#scroll-to-top').fadeOut();
+			}
+		});
+
+		$('#scroll-to-top').click(function () {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+
+	});
+
+
+	//dropdown
+	const $dropdown = $(".dropdown");
+	const $dropdownToggle = $(".dropdown-toggle");
+	const $dropdownMenu = $(".dropdown-menu");
+	const showClass = "show";
+
+	$(window).on("load resize", function () {
+		if (this.matchMedia("(min-width: 768px)").matches) {
+			$dropdown.hover(
+				function () {
+					const $this = $(this);
+					$this.addClass(showClass);
+					$this.find($dropdownToggle).attr("aria-expanded", "true");
+					$this.find($dropdownMenu).addClass(showClass);
+				},
+				function () {
+					const $this = $(this);
+					$this.removeClass(showClass);
+					$this.find($dropdownToggle).attr("aria-expanded", "false");
+					$this.find($dropdownMenu).removeClass(showClass);
+				}
+			);
+		} else {
+			$dropdown.off("mouseenter mouseleave");
+		}
+	});
+
+
+
+}(jQuery));
+
+$(function () {
+	$("#addClass").click(function () {
+		$('#qnimate').addClass('popup-box-on');
+	});
+
+	$("#removeClass").click(function () {
+		$('#qnimate').removeClass('popup-box-on');
+	});
+})
+
+function openCity(evt, cityName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		/* Toggle between adding and removing the "active" class,
+		to highlight the button that controls the panel */
+		this.classList.toggle("active");
+
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
+
+	//tooltips
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+}
